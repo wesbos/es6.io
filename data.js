60bb049598673f5be0ce18d37c8fb3f568f6e704
wesbos <wesbos@gmail.com>
2016-07-17 22:48:14 -0400

    🔥

---
+++ data.js
@@ -0,0 +1,51 @@
+/* eslint no-unused-vars: 0 */
+
+/*
+This is a file filled with dummy data we can use in our examples.
+Just copy/paste the data that you need from this file into your file.
+*/
+
+/*
+  A list of inventors
+*/
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879 },
+  { first: 'Isaac', last: 'Newton', year: 1643 },
+  { first: 'Galileo', last: 'Galilei', year: 1564 },
+  { first: 'Marie', last: 'Curie', year: 1867 },
+  { first: 'Johannes', last: 'Kepler', year: 1571 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473 },
+  { first: 'Max', last: 'Planck', year: 1858 },
+];
+
+/*
+  Beers
+*/
+const beers = [
+  { name: 'Purple Iris', abv: 6.8, label: 'https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png', type: 'IPA' },
+  { name: 'Orange Blossom Pilsner', abv: 5.5, label: 'https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png', type: 'Pilsner' },
+  { name: 'Darkness', abv: 4.2, label: 'https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png', type: 'Stout' },
+  { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
+  { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
+];
+
+/*
+  Pizza Types
+*/
+const pizzas = ['Deep Dish', 'Peperoni', 'Hawaiian', 'Meatzza', 'Spicy Mama', 'Margherita'];
+
+/*
+  Cuts of Beef
+*/
+
+/* eslint-disable max-len */
+const cuts = ['Chuck', 'Brisket', 'Shank', 'Rib', 'Plate', 'Flank', 'Short Loin', 'Tenderloin', 'Sirloin', 'Round'];
+/* eslint-enable max-len */
+
+
+const inventory = {
+  shoes: 10,
+  socks: 6,
+  shirts: 10,
+  pants: 5,
+}
