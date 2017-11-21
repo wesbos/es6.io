52788ce899b1dab8e8ee9c6262fc0e9a3fad5f63
mwood <matt@cratebind.com>
2017-11-21 14:58:18 -0600

    changed babel to babel-loader

--- 13 - JavaScript Modules and Using npm/es6modules-FINISHED/webpack.config.js
+++ 13 - JavaScript Modules and Using npm/es6modules-FINISHED/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015-native-modules']
         }
